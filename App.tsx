61d3fa90280d5532a7ab12e7c161fb4c83ea5d20
t-yng <t.yng.greenmonkey@gmail.com>
2019-02-27 23:11:47 +0900

    ActionSheetを追加

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Header } from 'react-native-elements';
 import SearchBox from './components/SearchBox';
+import ActionSheet from 'react-native-actionsheet';
 
 export default class App extends React.Component {
+  private actionSheet: any;
+
+  showActionSheet = () => {
+    this.actionSheet.show();
+  }
+
+  onPressActionSheetButton = (index: number) => {
+    /* do something */
+  }
+
   render() {
     return (
       <View>
@@ -12,6 +23,17 @@ export default class App extends React.Component {
           centerComponent={{text: '本棚'}}
         />
         <SearchBox/>
+        <Button
+          title="登録"
+          onPress={this.showActionSheet}
+        />
+        <ActionSheet
+          ref={(o: any) => this.actionSheet = o}
+          options={['検索結果から登録', 'Cancel']}
+          cancelButtonIndex={1}
+          destructiveButtonIndex={1}
+          onPress={this.onPressActionSheetButton}
+        />
       </View>
     );
   }
