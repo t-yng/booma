e67cf94909d960ab39e139e2f0f04903a6e2f138
t-yng <t.yng.greenmonkey@gmail.com>
2019-03-05 20:50:21 +0900

    本の一覧表示をスクロールビューに変更

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BookListItem from './BookListItem';
 import Book from '../lib/Book';
+import { ScrollView } from 'react-native';
 
 interface Props {
   books: Array<Book>,
@@ -10,11 +11,11 @@ const BookList: React.SFC<Props> = ({
   books
 }) => {
   return (
-    <>
+    <ScrollView>
       {books.map((book, i) => {
         return　<BookListItem key={i} book={book}/>
       })}
-    </>
+    </ScrollView>
   );
 }
 
